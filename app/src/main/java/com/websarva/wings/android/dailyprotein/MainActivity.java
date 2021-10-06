740b431a69c4ff42d435ca8c020175fa21e3fbbb
Ogawa222 <72852366+Ogawa222@users.noreply.github.com>
2021-10-07 00:10:06 +0900

    時刻入力機能を追加

--- app/src/main/java/com/websarva/wings/android/dailyprotein/MainActivity.java
+++ app/src/main/java/com/websarva/wings/android/dailyprotein/MainActivity.java
@@ -1,14 +1,59 @@
 package com.websarva.wings.android.dailyprotein;
 
+import androidx.fragment.app.FragmentActivity;
+import android.app.DatePickerDialog.OnDateSetListener;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Locale;
+
+
+public class MainActivity extends AppCompatActivity // AppCompatActivityをFragmentActivityにするとタイトルバーが非表示になる
+        implements OnDateSetListener, TimePickerDialog.OnTimeSetListener{
+
+    private TextView textViewDate;
+    private TextView textViewTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textViewDate = findViewById(R.id.tvDate);
+        textViewTime = findViewById(R.id.tvTime);
+    }
+
+    // DatePickerフラグメントを使い日付のセット
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int day) {
+        // フォーマットを日本語にしてセット
+        String str = String.format(Locale.JAPAN, "%d年%d月%d日", year, month+1, day+1);
+        textViewDate.setText(str);
+    }
+
+    public void showDatePickerDialog(View view){
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "datePicker");
+    }
+
+    @Override
+    public void onTimeSet(TimePicker view, int hour, int minute) {
+
+        String str = String.format(Locale.US, "%d:%d", hour, minute);
+        textViewTime.setText( str );
+
+    }
+
+    public void showTimePickerDialog(View v) {
+        DialogFragment newFragment = new TimePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "timePicker");
+
     }
 }
